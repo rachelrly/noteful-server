d43be2068cae7b218a1cb9f468755bfad8562ea8
Rachel Reilly <r.a.reill18@gmail.com>
2020-09-29 16:20:36 -0500

    DATABASE now

--- src/config.js
+++ src/config.js
@@ -1,5 +1,5 @@
 module.exports = {
     PORT: process.env.PORT || 8000,
     NODE_ENV: process.env.NODE_ENV || 'developemnt',
-    DB_URL: process.env.DB_URL || 'postgresql://rachel:1234@localhost/noteful'
+    DATABASE_URL: process.env.DATABASE_URL || 'postgresql://rachel:1234@localhost/noteful'
 }
